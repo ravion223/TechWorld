dfd30b950437cc44e781e04039f6f093939a4793
Rostyslav <tablettaoson2016@gmail.com>
2024-08-29 19:46:34 +0300

    Created main_page vies and fixed some problems connected with settings

---
+++ main_page/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.MainPageView.as_view(), name='main-page')
+]
--- main_page/views.py
+++ main_page/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
+
+class MainPageView(generic.TemplateView):
+    template_name = 'main_page/main-page.html'
