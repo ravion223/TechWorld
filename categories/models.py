ebbb41d978983d8b54b4588839d9556f886f37e2
Rostyslav <tablettaoson2016@gmail.com>
2024-09-04 18:40:33 +0300

    Created categories app and set models up there

---
+++ categories/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return f"Category - {self.name}"
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, related_name='products')
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.IntegerField()
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+
+    content = models.TextField()
+    rate = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ]
+    )
--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main_page',
     'authentication_system',
+    'categories',
 ]
 
 MIDDLEWARE = [
